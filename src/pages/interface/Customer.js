3eb4aa17b9931dba81c99a63b3c84afccaf64b86
Joseph Diza <josephm.diza@gmail.com>
2025-07-31 05:01:09 -0700

    Create preliminary login page
    Rename AppBar -> TopAppBar

--- src/pages/interface/Customer.js
+++ src/pages/interface/Customer.js
@@ -8,7 +8,7 @@ import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 import Container from '@mui/material/Container';
 
-import ResponsiveAppBar from '../../components/AppBar';
+import ResponsiveAppBar from '../../components/TopAppBar';
 
 import './Login.css';
 import BottomAppBar from '../../components/BotAppBar';
@@ -18,17 +18,7 @@ import BottomAppBar from '../../components/BotAppBar';
 const Customer = () => {
   const [value, setValue] = React.useState(0);
   return (
-    <div>
-
-
-      {/* <Container class="container" className="login-form"> */}
-      {/* <Box class="login-form" sx={{ margin: 2 }} */}
-      {/*     display="flex" */}
-      {/*     justifyContent="center" */}
-      {/*     alignItems="center" */}
-      {/* > */}
-
-
+    <>
       <Stack
         sx={{
             justifyContent: "center",
@@ -36,7 +26,7 @@ const Customer = () => {
             gap: 6,
         }}
       >
-        <ResponsiveAppBar></ResponsiveAppBar>
+        <ResponsiveAppBar/>
 
         <Stack
           spacing={2}
@@ -100,53 +90,11 @@ const Customer = () => {
             >
               Register
             </Button>
-          {/* </Grid> */}
           </Stack>
         </Stack>
-        <BottomAppBar></BottomAppBar>
-
-        {/* <BottomNavigation */}
-        {/*   showLabels */}
-        {/*   value={value} */}
-        {/*   onChange={(event, newValue) => { */}
-        {/*     setValue(newValue); */}
-        {/*   }} */}
-        {/* > */}
-        {/*   <BottomNavigationAction label="Recents" icon={<RestoreIcon />} /> */}
-        {/*   <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} /> */}
-        {/*   <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} /> */}
-        {/* </BottomNavigation> */}
+        <BottomAppBar/>
       </Stack>
-
-      {/* </Box> */}
-      {/* </Container> */}
-
-      <h2>Account Information </h2>
-
-      <p>Here is where we will show the customer's account information</p>
-      <ul>
-        <li> View Account Profile </li>
-        <li> Edit Account Details </li>
-      </ul>
-
-      <h2>Invoices</h2>
-      <p>Here is where we will show the customer's transaction information and history </p>
-
-      <h2>Repair History</h2>
-      <p>Here is where we will show the current and previous device repairs history </p>
-
-      <h2> Frontend Roadmap </h2>
-      <ol>
-        <li> Determine requirements of all users </li>
-        <li> Sketch ui/ux wireframe for all interfaces and forms </li>
-        <li> Design components </li>
-        <li> Style components </li>
-        <li> Create front-end data structures for back-end model data </li>
-        <li> Integrate front-end with backend using HTTP requests via Axiom or fetch </li>
-      </ol>
-
-
-    </div>
+    </>
   );
 };
 
