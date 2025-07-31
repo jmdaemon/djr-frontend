6fc9b9fd29ec4ae042cbcddef19b7fe02daa023d
Joseph Diza <josephm.diza@gmail.com>
2025-07-31 04:37:38 -0700

    Prototype initial customer login
    Make Register & Login btns same size
    
    Design initial app bars

--- src/pages/interface/Customer.js
+++ src/pages/interface/Customer.js
@@ -1,8 +1,125 @@
-import React from 'react';
+import * as React from 'react';
+
+import { TextField, Button, FormControl, InputLabel, Input, Box, Avatar, Stack, Grid, BottomNavigation, BottomNavigationAction } from '@mui/material';
+
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+
+import Container from '@mui/material/Container';
+
+import ResponsiveAppBar from '../../components/AppBar';
+
+import './Login.css';
+import BottomAppBar from '../../components/BotAppBar';
+
+/* <h1>Service Repair App</h1> */
+
 const Customer = () => {
+  const [value, setValue] = React.useState(0);
   return (
     <div>
-      <h1>Customer Interface</h1>
+
+
+      {/* <Container class="container" className="login-form"> */}
+      {/* <Box class="login-form" sx={{ margin: 2 }} */}
+      {/*     display="flex" */}
+      {/*     justifyContent="center" */}
+      {/*     alignItems="center" */}
+      {/* > */}
+
+
+      <Stack
+        sx={{
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 6,
+        }}
+      >
+        <ResponsiveAppBar></ResponsiveAppBar>
+
+        <Stack
+          spacing={2}
+          direction="column"
+          sx={{
+            width: 300,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          width="800"
+        >
+          {/* <h1>Account Login</h1> */}
+
+          <Avatar
+            src="/broken-image.jpg"
+            sx={{ width: 300, height: 300 }}
+          />
+
+          <h1>Login</h1>
+
+          {/* <TextField label="Email" color="primary" width="200"> */}
+          {/*   Email */}
+          {/* </TextField> */}
+
+          <FormControl variant="standard" fullWidth>
+            <InputLabel htmlFor="component-simple">Email</InputLabel>
+            <Input id="component-simple" defaultValue=""/>
+          </FormControl>
+
+
+          <FormControl variant="standard" fullWidth>
+            <InputLabel htmlFor="component-simple">Password</InputLabel>
+            <Input id="component-simple" defaultValue=""/>
+          </FormControl>
+
+          {/* <TextField label="Password" color="primary" width="200"> */}
+          {/*   Password */}
+          {/* </TextField> */}
+          
+          
+          {/* <Grid container spacing={2}> */}
+          <Stack spacing={2}>
+            <Button
+              size="large"
+              variant="contained"
+              color="primary"
+              sx={{
+                width: 300,
+              }}
+            >
+              Login
+            </Button>
+
+            <Button
+              size='small'
+              variant='outlined'
+              color='primary'
+              sx={{
+                width: 300,
+              }}
+            >
+              Register
+            </Button>
+          {/* </Grid> */}
+          </Stack>
+        </Stack>
+        <BottomAppBar></BottomAppBar>
+
+        {/* <BottomNavigation */}
+        {/*   showLabels */}
+        {/*   value={value} */}
+        {/*   onChange={(event, newValue) => { */}
+        {/*     setValue(newValue); */}
+        {/*   }} */}
+        {/* > */}
+        {/*   <BottomNavigationAction label="Recents" icon={<RestoreIcon />} /> */}
+        {/*   <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} /> */}
+        {/*   <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} /> */}
+        {/* </BottomNavigation> */}
+      </Stack>
+
+      {/* </Box> */}
+      {/* </Container> */}
 
       <h2>Account Information </h2>
 
