afb7881e7d872ae0e2f0ed66e3863f98cb205f90
Joseph Diza <josephm.diza@gmail.com>
2025-07-31 11:36:45 -0700

    Add button to request device servicing

--- src/components/Repairs.tsx
+++ src/components/Repairs.tsx
@@ -6,6 +6,7 @@ import BottomAppBar from './BotAppBar';
 import RequestsTable from './RepairRequests';
 import UserNav from './UserNav';
 import RepairsTable from './RepairsTable';
+import {Link} from 'react-router-dom';
 
 const Repairs = () => {
   const [value, setValue] = React.useState(0);
@@ -29,9 +30,23 @@ const Repairs = () => {
             <UserNav/>
           </Grid>
 
-          <Grid size={4}>
+          <Grid size={4} spacing={2}>
             <h2>Service Requests</h2>
             <RequestsTable/>
+
+            <Link to="/customer/register" style={{ color: 'inherit', textDecoration: 'none' }}>
+              <Button
+                size='small'
+                variant='contained'
+                color='primary'
+                sx={{
+                    marginTop: 2,
+                }}
+              >
+                Request Device Servicing
+              </Button>
+            </Link>
+
           </Grid>
 
           <Grid size={4}>
